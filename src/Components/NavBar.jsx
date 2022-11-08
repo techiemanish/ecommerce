9a1c5a2cc3b7086810cba2b6e4cd7e4e8011c1df
techiemanish <manishtiwari627@gmail.com>
2022-11-08 22:50:41 +0530

    Responsive NavBar created

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,7 +6,7 @@ import FilledCart from './cart/FilledCart'
 function NavBar(props) {
   return (
     <>   
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="sticky top-0 bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
             <a href="/" className="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Logo"/>
