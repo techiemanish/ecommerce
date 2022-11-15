3b2f7ffd882d4f175be55f5e6bfb86a4fd42ff16
techiemanish <manishtiwari627@gmail.com>
2022-11-15 23:37:30 +0530

    Added seller check in signup

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -28,7 +28,11 @@ function Register(props) {
     <div class="mb-6">
         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
         <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required=""/>
-    </div> 
+    </div>
+    <div class="flex items-center mb-6 pl-2 rounded border border-gray-200 dark:border-gray-700">
+    <input id="bordered-checkbox-1" type="checkbox" value="true" name="bordered-checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+    <label for="bordered-checkbox-1" class="py-2 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">Seller Account</label>
+    </div>
     <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
     <div className="mt-4 text-sm font-medium text-gray-500 dark:text-gray-300">
             Already have account? <Link to={"/login"} className="text-blue-700 hover:underline dark:text-blue-500">Sign in</Link>
